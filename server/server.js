2e0783ba820e9e4a69e1936bcca56fc171b7a143
Khushdev Yogi <khushdevyogi001@gmail.com>
2023-06-22 19:04:14 +0530

    cors added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const app = express();
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 
 // Securing data with dotenv
